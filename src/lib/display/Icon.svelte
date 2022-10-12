97e7204c5fdf0c08ea382aaf6c1197476cc7ad2f
Albert <albert@framework.tools>
2022-10-12 20:25:41 +1030

    Push latest autonomous zones update

---
+++ src/lib/display/Icon.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+import type { SvelteComponent } from "svelte"
+
+export let icon: typeof SvelteComponent
+</script>
+<div class="icon">
+    <svelte:component this={icon}/>
+</div>
+<style lang="stylus">
+.icon
+    display inline-flex
+</style>
---
+++ src/lib/display/Logo.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+import Flag from "svelte-material-icons/FlagOutline.svelte"
+
+</script>
+<a href="/" class="logo">
+    <div class="icon">
+        <Flag/>
+    </div>
+    <div class="text">
+        AUTONOMOUS ZONES
+    </div>
+</a>
+<style lang="stylus">
+@import "variables"
+
+.logo
+    display inline-flex
+    align-items center
+    color white
+    text-decoration none
+    font-weight 600
+    letter-spacing 2px
+    font-size 16px
+    padding 8px
+    gap 8px
+    .icon
+        display inline-flex
+        color $brand
+        font-size 24px
+
+</style>
